54328023e16396c7815b2961f5e06ac3e36c1b77
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-05-02 01:41:39 +0800

    Disable threads.

--- time_tests.sh
+++ time_tests.sh
@@ -8,16 +8,17 @@ if [ "$TEST" = "" ]; then
 fi
 
 test() {
-	$TEST --nocapture $1 2>&1 | grep "duration = " | sed -e "s/duration = \(.*\)/\1 $1/g"
+	$TEST --nocapture --test-threads 1 $1 2>&1 | grep "duration = " | sed -e "s/test \(.*\) ... duration = \(.*\)/\2 \1/g"
 }
 
 # Run tests and sort by time.
 (
 test closure_tree::counter_loop
+test closure_s1vm::counter_loop
 test closure_reg0::counter_loop
 test switch::counter_loop
 test closure_block::counter_loop
-test closure_tail_2::counter_loop
+#test closure_tail_2::counter_loop
 test fused::ct3::counter_loop
 test switch_2::counter_loop
 test fused::rt3::counter_loop
@@ -26,9 +27,9 @@ test fused::rt2::counter_loop
 test enum_tree_2::counter_loop
 #test switch::more_comps
 test fused::rt::counter_loop
-test switch_tail::counter_loop
+#test switch_tail::counter_loop
 test closure_loop::counter_loop
-test switch_tail_2::counter_loop
+#test switch_tail_2::counter_loop
 test enum_tree::counter_loop
 #test closure_block::more_comps
 test closure_tail::counter_loop
